c54dc11911640bb92283f72df3caa2ef628e5f99
RhysC <rhysevancampbell@gmail.com>
2015-01-21 15:48:30 +0800

    splitting config out, that file should be the only thing required to change for diffrent field sizes

---
+++ src/javascripts/config.js
@@ -0,0 +1,18 @@
+var config = (function(){
+   
+  var multiplier = 30;
+  var poolWidth = 20;
+  var poolLength = 30;
+  var goalDepth = 0.5;
+  var goalWidth = 3;    
+  
+  return{
+    multiplier : multiplier,
+    poolWidth : poolWidth,
+    poolLength : poolLength,
+    goalDepth : goalDepth,
+    goalWidth : goalWidth,
+    CANVAS_WIDTH :(poolLength * multiplier) + (goalDepth * multiplier),
+    CANVAS_HEIGHT : poolWidth * multiplier
+  }
+})();
