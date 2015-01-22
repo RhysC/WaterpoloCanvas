2cf75f788d10d212676351e98ffb751ff2681839
RhysC <rhysevancampbell@gmail.com>
2015-01-22 13:39:21 +0800

    Pulling out concerns, ie the drawing and knowing about multipliers etc is not relavent to the entities. There is an issue with an implicit global of canvas too which may need addressing

--- src/javascripts/entities.js
+++ src/javascripts/entities.js
@@ -1,32 +1,83 @@
-var entities = (function() {
-          		
+var canvasHelpers = (function(){
+  var drawYAxisLine = function(x, color){
+    canvas.beginPath();
+    canvas.moveTo(x*config.multiplier, 0); 
+    canvas.lineTo(x*config.multiplier, config.CANVAS_HEIGHT); 
+    canvas.strokeStyle = color;
+    canvas.stroke();
+  }
+
+  var drawRect = function(x, y, length, height){
+    canvas.fillRect(
+      (x * config.multiplier), 
+      (y * config.multiplier), 
+      (length * config.multiplier), 
+      (height * config.multiplier));    
+  }
+
+  var drawSprite = function(position,sprite){
+    var x = position.getX() * config.multiplier;
+    var y = position.getY() * config.multiplier;
+    sprite.draw(canvas, x, y);
+  }
+  return {
+    drawYAxisLine:drawYAxisLine,
+    drawRect:drawRect,
+    drawSprite: drawSprite
+  };
+})();
+
+var entities = (function(field) {
+  var FieldPosition = function(x,y){
+    this.getX = function(){ return x; };
+    this.getY = function(){ return y; };
+  };
+  
   var PositionBoundary = function(x,color){
-    var self = this;
-    self.x = x*config.multiplier;
-    self.color = color;
-    self.draw = function(){
-      canvas.beginPath();
-      canvas.moveTo(self.x, 0); 
-      canvas.lineTo(self.x, config.CANVAS_HEIGHT); 
-      canvas.strokeStyle = self.color;
-      canvas.stroke();
+    this.draw = function(){
+      canvasHelpers.drawYAxisLine(x,color);    
     }
   }
+  
   var Goal = function(x){
     var self = this;
-    self.color= "#00A";
-    self.getX = function(){ return x; };
-    self.getY = function(){ return (config.poolWidth/2); };
+    self.fieldPosition = new FieldPosition(x, config.poolWidth/2);
     self.draw = function(){
-       canvas.fillRect((x * config.multiplier), 
-                       (config.CANVAS_HEIGHT/2)-(config.goalWidth*config.multiplier/2), 
-                       (config.goalDepth*config.multiplier), 
-                       (config.goalWidth*config.multiplier));
+      canvasHelpers.drawRect(x,(config.poolWidth/2)-(config.goalWidth/2),config.goalDepth,config.goalWidth);
     }					
   }
+   
+  //IN-FIELD ELEMENTS
+  var InFieldEntity = function(x,y,sprite){
+    var self = this;
+    self.fieldPosition = new FieldPosition(x,y);
+    self.sprite = sprite;   
+    
+    self.setPosition = function(x, y) {
+      self.fieldPosition = new FieldPosition(x,y);
+    };
+    self.goalSideOf = function(goal, player, distanceFromPlayer) {
+      var point = field.getPointBetween(goal.fieldPosition, player.fieldPosition, distanceFromPlayer);
+      self.setPosition(point.x,point.y);
+    };
+    
+    self.draw = function() {
+      canvasHelpers.drawSprite(self.fieldPosition, self.sprite);
+    };  
+  }
   
+  var createBluePlayer = function(x,y){
+    return new InFieldEntity(x, y, Sprite("enemy"));    
+  }
+  var createWhitePlayer = function(x,y){
+     return new InFieldEntity(x, y, Sprite("player"));    
+  }   
+  var createBall = function(x,y){
+     return new InFieldEntity(x, y, Sprite("ball"));    
+  }     
+     
   var goal1 = new Goal(0);
-  var goal2 = new Goal(config.poolLength-config.goalDepth);
+  var goal2 = new Goal(config.poolLength);
        
   var first2Meter = new PositionBoundary(2, "#f00");
   var second2Meter = new PositionBoundary(config.poolLength -2, "#f00");
@@ -44,50 +95,12 @@ var entities = (function() {
     goal2.draw();
   }
   
-  var InFieldEntity = function(x,y,sprite){
-    var self = this;
-    self.field = { x:x, y:y };
-    self.getX = function(){ return self.field.x * config.multiplier;};
-    self.getY = function(){ return self.field.y * config.multiplier;};
-    self.sprite = sprite;    
-    self.draw = function() {
-      self.sprite.draw(canvas, self.getX(), self.getY());
-    };
-    self.setPosition = function(x, y) {
-      self.field.x = x;
-      self.field.y = y;
-    };
-    self.goalSideOf = function(goal, player) {
-      var distanceFromPlayer = 0.5;
-      console.log(goal);
-      var opposite = goal.getY() - player.field.y;
-      var adjacent = goal.getX() - player.field.x;
-      var angle = Math.atan(opposite/adjacent);
-      var hypotenuseLength = Math.sqrt((opposite*opposite)+(adjacent*adjacent));
-      var goalSideLength = hypotenuseLength - distanceFromPlayer;
-      
-      self.field.x = goal.getX() + (Math.cos(angle)*goalSideLength);
-      self.field.y = goal.getY() + (Math.sin(angle)*goalSideLength);
-    };
-  }
-  
-  var BluePlayer = function(x,y){
-    InFieldEntity.call(this, x, y, Sprite("enemy"));    
-  }
-  var WhitePlayer = function(x,y){
-    InFieldEntity.call(this, x, y, Sprite("player"));    
-  }   
-  var Ball = function(x,y){
-    InFieldEntity.call(this, x, y, Sprite("ball"));    
-  }     
   return {
-    BluePlayer : BluePlayer,
-    WhitePlayer : WhitePlayer,
-    Ball: Ball,
+    createBluePlayer : createBluePlayer,
+    createWhitePlayer : createWhitePlayer,
+    createBall: createBall,
     goal1:goal1,
     goal2:goal2,
-    drawField : drawField, 
-    CANVAS_WIDTH : config.CANVAS_WIDTH,
-    CANVAS_HEIGHT : config.CANVAS_HEIGHT
+    drawField : drawField    
   }
-}());
+}(fieldPositioning));
