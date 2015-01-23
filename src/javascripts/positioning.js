31ae2c8103c6d5704141ac15e47198fa5b73b04b
RhysC <rhysevancampbell@gmail.com>
2015-01-23 09:27:22 +0800

    was getting some crazy coords coming back, need to be able to move thes things around

--- src/javascripts/positioning.js
+++ src/javascripts/positioning.js
@@ -1,14 +1,14 @@
 var positioning = (function() {
 
   var getPointBetween = function(target, origin, distanceFromOrigin) {
-    var opposite = target.getY() - origin.getY();
-    var adjacent = target.getX() - origin.getX();
+    var opposite =  Math.abs(target.getY() - origin.getY());
+    var adjacent =  Math.abs(target.getX() - origin.getX());
     var angle = Math.atan(opposite/adjacent);
     var hypotenuseLength = Math.sqrt((opposite*opposite)+(adjacent*adjacent));
     var newHypotenuseLength = hypotenuseLength - distanceFromOrigin;
 
-    var newX = target.getX() + (Math.cos(angle)*newHypotenuseLength);
-    var newY = target.getY() + (Math.sin(angle)*newHypotenuseLength);
+    var newX = Math.min(target.getX(), origin.getX()) + (Math.cos(angle)*newHypotenuseLength);
+    var newY = Math.min(target.getY(), origin.getY()) + (Math.sin(angle)*newHypotenuseLength);
     return {
       x: newX,
       y: newY
