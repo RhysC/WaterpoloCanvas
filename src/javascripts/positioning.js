afe3b42273817340e68e8d2ba6be7c8380eec5d5
RhysC <rhysevancampbell@gmail.com>
2015-01-23 11:49:21 +0800

    correcting so we are always closer to the origin, previsous we were just closest to the 'lowest' point

--- src/javascripts/positioning.js
+++ src/javascripts/positioning.js
@@ -5,10 +5,25 @@ var positioning = (function() {
     var adjacent =  Math.abs(target.getX() - origin.getX());
     var angle = Math.atan(opposite/adjacent);
     var hypotenuseLength = Math.sqrt((opposite*opposite)+(adjacent*adjacent));
-    var newHypotenuseLength = hypotenuseLength - distanceFromOrigin;
-
-    var newX = Math.min(target.getX(), origin.getX()) + (Math.cos(angle)*newHypotenuseLength);
-    var newY = Math.min(target.getY(), origin.getY()) + (Math.sin(angle)*newHypotenuseLength);
+    var newHypotenuseLength = 0;hypotenuseLength - distanceFromOrigin;
+    var newX, 
+        newY;
+    if(origin.getX() > target.getX()){
+      var newHypotenuseLength = hypotenuseLength - distanceFromOrigin;
+      var newX = Math.min(target.getX(), origin.getX()) + (Math.cos(angle)*newHypotenuseLength);
+    }else{
+      var newHypotenuseLength = distanceFromOrigin;
+      var newX = Math.min(target.getX(), origin.getX()) + (Math.cos(angle)*newHypotenuseLength);
+    }
+    
+    if(origin.getY() > target.getY()){
+      var newHypotenuseLength = hypotenuseLength - distanceFromOrigin;
+      var newY = Math.min(target.getY(), origin.getY()) + (Math.sin(angle)*newHypotenuseLength);
+    }else{
+      var newHypotenuseLength = distanceFromOrigin;
+      var newY = Math.min(target.getY(), origin.getY()) + (Math.sin(angle)*newHypotenuseLength);
+    }
+       
     return {
       x: newX,
       y: newY
